aa755e1c3a4d1e94b8903da0af16624a5b718c1e
cpringle34 <52711377+cpringle34@users.noreply.github.com>
2024-04-25 12:29:24 -0400

    Tutorial Bounds issue fixed

--- Time Signature/Assets/Scripts/Tutorial.cs
+++ Time Signature/Assets/Scripts/Tutorial.cs
@@ -30,6 +30,7 @@ public class Tutorial : MonoBehaviour
         rb = player.GetComponent<CharacterMovement>();
         inputManager = new InputManager();
         inputManager.Enable();
+        dialogueRunner.Stop();
         StartConversation();
 
     }
